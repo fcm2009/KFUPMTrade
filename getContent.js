e6bd94b52b7c12821f82b36d6d85192c965e66c1
fcm2009 <fcm2009@gmail.com>
2015-05-12 20:12:50 +0300

    before editing getContent()

--- getContent.js
+++ getContent.js
@@ -2,25 +2,8 @@
  * Created by fcm2009 on 5/11/15.
  */
 
-var server;
+function showContent() {
 
-function ready() {
-    if(server.readyState == 4 && server.status == 200) {
-        document.write(server.responseText);
-    }
 }
 
-function getContent(category) {
-    try {
-        server = new XMLHttpRequest();
-        server.open("POST", "test.php", true);
-        server.addEventListener("readystatechange", ready, false);
-        server.setRequestHeader("Content-type","application/x-www-form-urlencoded")
-        server.send("data=" + "a");
-    }
-    catch (exception) {
-        document.write("********");
-    }
-}
-
-getContent("All");
+showContent("All");
