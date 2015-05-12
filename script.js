52dfb83c117adf912f294d50106678d656d4be4b
fcm2009 <fcm2009@gmail.com>
2015-05-12 20:21:44 +0300

    starting search

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
  * Created by Awad on 5/12/2015.
  */
 
+var asyncRequest;
 
-
-function doSearch(){
-
+function search() {
     try {
         var asyncRequest = new XMLHttpRequest();
-        asyncRequest.addEventListener("readystatechange", getContent, false);
+        asyncRequest.addEventListener("readystatechange", showContent, false);
         asyncRequest.open("POST", "search.php");
+        asyncRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");
         asyncRequest.send("keyword=" + keyword + "& category=" + selectedCateg);
     }
     catch(exception){
